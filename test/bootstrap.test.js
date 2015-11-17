3ee16fe0e1a8064958d2e92fdc3c2f1826bf832e
Jérémie Ledentu <jledentu@gmail.com>
2015-11-17 22:01:19 +0100

    Initialize tests

---
+++ test/bootstrap.test.js
@@ -0,0 +1,23 @@
+var Sails = require('sails'), sails;
+
+before(function(done) {
+
+  // Increase the Mocha timeout so that Sails has enough time to lift.
+  this.timeout(5000);
+
+  Sails.lift({
+    "hooks": {
+      "grunt": false
+    }
+  }, function(err, server) {
+    sails = server;
+    if (err) return done(err);
+    // here you can load fixtures, etc.
+    done(err, sails);
+  });
+});
+
+after(function(done) {
+  // here you can clear fixtures, etc.
+  Sails.lower(done);
+});
